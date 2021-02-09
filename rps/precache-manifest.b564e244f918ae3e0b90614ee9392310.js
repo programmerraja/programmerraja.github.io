e4402c7662080cd3595c42d3c6559d33ce5cab51
programmerraja <boooathis123@gmail.com>
2021-02-09 18:51:04 +0530

    Added RPS Game

---
+++ rps/precache-manifest.b564e244f918ae3e0b90614ee9392310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefaa0c0748b7e23cf67eb5238c5b0c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b311d2e5c1d59f146cfb",
+    "url": "./static/css/main.a20e383a.chunk.css"
+  },
+  {
+    "revision": "651aad9307a06467633f",
+    "url": "./static/js/2.0a128a14.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0a128a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b311d2e5c1d59f146cfb",
+    "url": "./static/js/main.ecba0ccc.chunk.js"
+  },
+  {
+    "revision": "3c22466f1092a61e015e",
+    "url": "./static/js/runtime-main.a2ac0524.js"
+  }
+]);
