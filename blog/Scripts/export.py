1f76a27d29c05d3e7db6b0ad1e43c02dc6301ef2
programmerraja <boooathis123@gmail.com>
2024-10-28 18:27:48 +0530

    Added more content

--- blog/Scripts/export.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from export_no_git import *
-
-print(f'git add {md_destination_path}/')
-os.system(f'git add {md_destination_path}/')
-os.system(f'git add {media_destination_path}/')
-os.system(f'git commit -m \"added article {compact_title}\"')
-os.system(f'git push origin')
